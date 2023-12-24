7142510f4223ed944b1d7c2a197edf9e56239819
kong <1400126414@qq.com>
2023-12-24 16:34:32 +0800

    Initial commit

---
+++ src/main/java/cn/hubindeveloper/middleware/Main.java
@@ -0,0 +1,9 @@
+package cn.hubindeveloper.middleware;/**
+ *@description：
+ *@author：Kong
+ *@date：${DATE}
+ */public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
